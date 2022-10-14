d8b5fc43b0189d5af4a263269e83817a477bf78e
0d1nss0n <115773776+0d1nss0n@users.noreply.github.com>
2022-10-14 05:57:30 +0000

    Create mc.py
    simple MAC changer

---
+++ mc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+interface = input("interface > ")
+new_mac = input("new MAC > ")
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("sudo ip link set dev " + interface + " down", shell=True)
+subprocess.call("sudo ip link set dev " + interface + " address " + new_mac, shell=True)
+subprocess.call("sudo ip link set dev " + interface + " up", shell=True)
+print("")
+print("MAC Address has been changed")
+print("")
+subprocess.call("sudo ip link show " + interface, shell=True)
+print("")
+print("Script Closed")
