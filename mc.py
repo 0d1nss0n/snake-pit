81ab4d8fd338b6ef1eee793e5952868d3cb101c0
0d1nss0n <115773776+0d1nss0n@users.noreply.github.com>
2022-10-14 06:39:48 +0000

    Update mc.py

--- mc.py
+++ mc.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 
 import subprocess
+import optparse
 
-interface = input("interface > ")
-new_mac = input("new MAC > ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 subprocess.call("sudo ip link set dev " + interface + " down", shell=True)
 subprocess.call("sudo ip link set dev " + interface + " address " + new_mac, shell=True)
 subprocess.call("sudo ip link set dev " + interface + " up", shell=True)
+
 print("")
 print("MAC address has been changed")
 print("")
+
 subprocess.call("sudo ip link show " + interface, shell=True)
+
 print("")
 print("Script Closed")
