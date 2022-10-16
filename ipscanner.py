57b291961f5f24a5bf2782f74230670340ae30c7
0d1nss0n <115773776+0d1nss0n@users.noreply.github.com>
2022-10-16 16:21:25 -0700

    Update ipscanner.py

--- ipscanner.py
+++ ipscanner.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 import scapy.all as scapy
-import optparse
+import argparse
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="Use -t to select subnet you want to scan. Example: -t 10.0.0.1/24")
-    (options, arguments) = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Use -t to select subnet you want to scan. Example: -t 10.0.0.1/24")
+    options = parser.parse_args()
     if not options.target:
         parser.error("[-] Please specify a subnet, use --help for more info")
     return options
 
+
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-
 
     client_list = []
     for element in answered_list:
@@ -30,7 +30,8 @@ def print_result(results_list):
     for client in results_list:
         print(client["ip"] + "\t\t" + client["mac"])
 
+
 options = get_arguments()
-        
+
 scan_result = scan(options.target)
 print_result(scan_result)
