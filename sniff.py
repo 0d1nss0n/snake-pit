8b507c5be0d6d009e5dea9e2e9304fe88e284770
0d1nss0n <115773776+0d1nss0n@users.noreply.github.com>
2022-10-19 18:46:52 -0700

    Create sniff.py

---
+++ sniff.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import scapy.all as scapy
+import optparse
+from scapy.layers import http
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="-i to select and interface")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, --help for more info")
+    return options
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(packet)
+
+
+options = get_arguments()
+
+sniff(options.interface)
